9232ab1bf07adf4e33f5e904a799cf7a6a76083f
Phuc <phuct1k61lvt@gmail.com>
2026-05-26 09:32:31 +0700

    back up FE

--- src/ai/genkit.ts
+++
@@ -1,9 +0,0 @@
-import { genkit } from 'genkit';
-import { googleAI } from '@genkit-ai/googleai'; // Import the constant
-
-export const ai = genkit({
-  plugins: [
-    googleAI({ apiKey: process.env.GEMINI_API_KEY }),
-  ],
-  model: 'googleai/gemini-2.0-flash',
-});
